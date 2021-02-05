2b80ec48dc794c1b7e8a57607213a75fcee67cd8
Koushik Pramanick <46644710+KOUSHIKDEVSTAR@users.noreply.github.com>
2021-02-05 23:22:01 +0530

    HERO

--- config/static.js
+++ config/static.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 const dbCon  = mysql.createConnection({
-    user :'root',
-    host : 'localhost',
-    password : 'admin',
-    database : 'mega_shop'
+    user :'uvkl3iwgrwdk5caf',
+    host : 'bpuoui6y5mdihzhpfuav-mysql.services.clever-cloud.com',
+    password : 'kjKh1ymarGlEmLVWHSxp',
+    database : 'bpuoui6y5mdihzhpfuav'
   })
   dbCon.connect( (error)=>{
     if(error){
